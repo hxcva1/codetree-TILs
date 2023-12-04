87af64f99533094ed9199c514a9cc100a519a1f3
Moonsun Park <hxcva1@gmail.com>
2023-12-04 23:37:01 +0900

    Title: 남녀노소 구분짓기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231204/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+
+if a:
+    if b >= 19:
+        print("WOMAN")
+    else:
+        print("GIRL")
+else:
+    if b >= 19:
+        print("MAN")
+    else:
+        print("BOY")
