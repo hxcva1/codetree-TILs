bcb1b51266892919c8c366c09b1b7e84f8a7775b
Moonsun Park <hxcva1@gmail.com>
2023-12-11 17:46:29 +0900

    Title: 369 게임, Time: 156ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231211/369 게임/369-game.py
+++ 231211/369 게임/369-game.py
@@ -4,7 +4,7 @@ for i in range(1, n + 1):
     flag = 1
     tmp = i
     while tmp:
-        if tmp % 10 == 3 or tmp % 10 == 6 or tmp % 10 == 9:
+        if tmp % 10 == 3 or tmp % 10 == 6 or tmp % 10 == 9 or tmp % 3 == 0:
             flag = 0
             break
         tmp /= 10
