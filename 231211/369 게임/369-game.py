01ec0f819620843cbb6c0cd65999e3d5187f1a43
Moonsun Park <hxcva1@gmail.com>
2023-12-11 17:50:51 +0900

    Title: 369 게임, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 231211/369 게임/369-game.py
+++ 231211/369 게임/369-game.py
@@ -7,7 +7,7 @@ for i in range(1, n + 1):
         if tmp % 10 == 3 or tmp % 10 == 6 or tmp % 10 == 9 or tmp % 3 == 0:
             flag = 0
             break
-        tmp /= 10
+        tmp //= 10
     if flag:
         print(i, end=" ")
     else:
