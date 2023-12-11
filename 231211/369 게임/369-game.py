7a76e8db4f217ec2e6070cb2817de6d0ed8ac740
Moonsun Park <hxcva1@gmail.com>
2023-12-11 17:45:43 +0900

    Title: 369 게임, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231211/369 게임/369-game.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(1, n + 1):
+    flag = 1
+    tmp = i
+    while tmp:
+        if tmp % 10 == 3 or tmp % 10 == 6 or tmp % 10 == 9:
+            flag = 0
+            break
+        tmp /= 10
+    if flag:
+        print(i, end=" ")
+    else:
+        print(0, end=" ")
