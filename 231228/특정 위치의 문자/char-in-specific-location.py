bc3e29eeabb195bb5c2eb675da040f8ea9200d5d
Moonsun Park <hxcva1@gmail.com>
2023-12-28 20:11:37 +0900

    Title: 특정 위치의 문자, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 231228/특정 위치의 문자/char-in-specific-location.py
+++ 231228/특정 위치의 문자/char-in-specific-location.py
@@ -2,7 +2,12 @@ word = ['L', 'E', 'B', 'R', 'O', 'S']
 
 ch = input()
 
+idx = -1
+
 for i, c in enumerate(word):
     if (c == ch):
         idx = i
-print(idx)
+if idx == -1:
+    print("None")
+else:
+    print(idx)
