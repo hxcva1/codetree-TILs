c3933fd4ca604c351fde41a5dcd206fd0d090d4c
Moonsun Park <hxcva1@gmail.com>
2024-01-04 17:23:40 +0900

    Title: n개의 숫자 중 최대 2개, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240104/n개의 숫자 중 최대 2개/two-max-of-n-num.py
+++ 240104/n개의 숫자 중 최대 2개/two-max-of-n-num.py
@@ -1,9 +1,10 @@
+import sys
 n = int(input())
 arr = list(map(int, input().split()))
 
 mx = max(arr)
 cnt = 0
-mx2 = arr[0]
+mx2 = -sys.maxsize
 for i in range(n):
     if (arr[i] == mx):
         cnt += 1
