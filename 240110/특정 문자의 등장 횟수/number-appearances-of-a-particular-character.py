d8c880ab91fbe006df35ea0daf655b9a91119fe0
Moonsun Park <hxcva1@gmail.com>
2024-01-10 17:10:26 +0900

    Title: 특정 문자의 등장 횟수, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240110/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240110/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -4,19 +4,15 @@ temp = string
 cnt1 = cnt2 = 0
 idx = temp.find('ee')
 while idx != -1:
-    if idx == len(temp):
-        break
     cnt1 += 1
-    temp = temp[idx+1 : -1]
+    temp = temp[idx+1 :]
     idx = temp.find('ee')
 
 temp = string
 idx = temp.find('eb')
 while idx != -1:
-    if idx == len(temp):
-        break
     cnt2 += 1
-    temp = temp[idx+1 : -1]
+    temp = temp[idx+1 :]
     idx = temp.find('eb')
 
 print(cnt1, cnt2)
