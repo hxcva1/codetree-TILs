6cb49ff48a64b620e250a6348a1786ad991ba858
Moonsun Park <hxcva1@gmail.com>
2024-01-18 15:57:01 +0900

    Title: 그 계절, 그 날, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240118/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,35 @@
+y, m, d = map(int, input().split())
+
+flag = 0
+#존재하면 계절 출력
+#윤년인가?
+def is_yoon(y):
+    if y % 4 == 0 and (y % 100 != 0 or y % 400 == 0):
+        return True
+    return False
+
+def season(m):
+    if m >= 3 and m <= 5:
+        return "Spring"
+    elif m >= 6 and m <= 8:
+        return "Summer"
+    elif m >= 9 and m <= 11:
+        return "Fall"
+    else:
+        return "Winter"
+
+if m == 2:
+    if is_yoon:
+        if d > 29:
+            flag = 1
+    else:
+        if d > 30:
+            flag = 1
+elif m == 4 or m == 6 or m == 9 or m == 11:
+    if d > 30:
+        flag = 1
+
+if flag:
+    print(-1)
+else:
+    print(season(m))
