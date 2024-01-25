52409b0ee22bd03e9f306232c3805c4692c53324
Moonsun Park <hxcva1@gmail.com>
2024-01-25 23:49:14 +0900

    Title: 오름 내림차순 정렬, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240125/오름 내림차순 정렬/inc-dec-sorting.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+arr.sort()
+for elem in arr:
+    print(elem, end=' ')
+print()
+arr = arr[::-1]
+for elem in arr:
+    print(elem, end=' ')
