7e8664a2637e3baafa2d080d5461e1505e280b17
Moonsun Park <hxcva1@gmail.com>
2024-01-26 13:05:13 +0900

    Title: 007, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240126/007/007.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__ (self, code, place, time):
+        self.code = code
+        self.place = place
+        self.time = time
+a = input().split()
+
+person1 = Person(a[0], a[1], a[2])
+print(f"secret code: {person1.code}")
+print(f"meeting point : {person1.place}")
+print(f"time : {person1.time}")
