9cb708718d6bc8e906ee217e42e65bcb23c1cfaf
Moonsun Park <hxcva1@gmail.com>
2024-01-29 16:21:07 +0900

    Title: 원점으로부터의 거리, Time: 261ms, Memory: 25MB, Status: Passed - CodeTree

--- 240129/원점으로부터의 거리/distance-from-origin.py
+++ 240129/원점으로부터의 거리/distance-from-origin.py
@@ -5,7 +5,7 @@ for i in range(n):
     x, y = tuple(map(int, input().split()))
     points.append((x,y, i+1))
 
-points.sort(key=lambda x : (x[0]+x[1], x[2]))
+points.sort(key=lambda x : (abs(x[0])+abs(x[1]), x[2]))
 
 for p in points:
     print(p[2])
