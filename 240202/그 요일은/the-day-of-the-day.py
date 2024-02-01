2220938d52221f923d9b7521fa9f79b395dd68e5
Moonsun Park <hxcva1@gmail.com>
2024-02-02 00:44:01 +0900

    Title: 그 요일은, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/그 요일은/the-day-of-the-day.py
+++ 240202/그 요일은/the-day-of-the-day.py
@@ -1,7 +1,7 @@
 m1, d1, m2, d2 = map(int, input().split())
 a = input()
 
-m = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+m = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 w = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 wdiff = 0
 for i in range(7):
@@ -18,7 +18,7 @@ for i in range(1, m2):
     day2 += m[i]
 day2 += d2
 diff = day2 - day1
-diff -= wdiff - 1
+diff -= wdiff
 ret = diff // 7
 if ret < 0:
     print(0)
