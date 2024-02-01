6ebc5d075b1a3d507456be805ef747e86afa847d
Moonsun Park <hxcva1@gmail.com>
2024-02-01 18:49:44 +0900

    Title: DateTime to DateTime, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/DateTime to DateTime/datetime-to-datetime.py
+++ 240201/DateTime to DateTime/datetime-to-datetime.py
@@ -1,7 +1,7 @@
 a, b, c = map(int, input().split())
 
-start = 60*24*11 + 11*60 + 11
-end = 60*24*a + 11*b + c
+start = (60*24*11) + (11*60) + 11
+end = (60*24*a) + (60*b) + c
 if end - start < 0:
     print(-1)
 else:
