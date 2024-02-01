2f9a04add12c0968a6b4fc0d2318359cfa113a2a
Moonsun Park <hxcva1@gmail.com>
2024-02-01 18:38:43 +0900

    Title: Date to Date, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240201/Date to Date/date-to-date.py
+++ 240201/Date to Date/date-to-date.py
@@ -3,7 +3,7 @@ a, b, c, d = map(int, input().split())
 m = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 days = 0
-for i in range(c-1, a, -1):
+for i in range(a+1, c):
     days += m[i]
 days += d
 days += m[a]-b+1
