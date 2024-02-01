dbd223e6b5b70c2d974b60e6fac80170c3b7fd0c
Moonsun Park <hxcva1@gmail.com>
2024-02-01 18:40:32 +0900

    Title: Date to Date, Time: 107ms, Memory: 25MB, Status: Passed - CodeTree

--- 240201/Date to Date/date-to-date.py
+++ 240201/Date to Date/date-to-date.py
@@ -5,6 +5,9 @@ m = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = 0
 for i in range(a+1, c):
     days += m[i]
-days += d
-days += m[a]-b+1
+if a == c:
+    days += d - b + 1
+else:
+    days += d
+    days += m[a]-b+1
 print(days)
