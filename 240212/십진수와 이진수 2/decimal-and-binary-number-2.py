47a38a3c8d5490658a231ea8194e993ab3e425fe
Moonsun Park <hxcva1@gmail.com>
2024-02-12 15:24:40 +0900

    Title: 십진수와 이진수 2, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240212/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -0,0 +1,14 @@
+n = list(input())
+
+ret = 0
+for elem in n:
+    ret = ret * 2 + int(elem)
+ret *= 17
+
+def f(n):
+    if n < 2:
+        print(n%2, end='')
+        return
+    f(n//2)
+    print(n%2)
+f(ret)
