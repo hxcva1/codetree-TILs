556e2141e15428c3858d44ad25e8518e87cc6937
Moonsun Park <hxcva1@gmail.com>
2024-02-13 21:27:10 +0900

    Title: 만나는 그 순간, Time: 123ms, Memory: 40MB, Status: Wrong Answer - CodeTree

---
+++ 240213/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,41 @@
+n, m = tuple(map(int, input().split()))
+
+a = [0] * (1000000)
+b = [0] * (1000000)
+
+pos = 0
+i = 0
+for _ in range(n):
+    d, t = input().split()
+    if d == "R":
+        for j in range(int(t)):
+            a[i] = pos
+            pos += 1
+            i += 1
+    else:
+        for j in range(int(t)):
+            a[i] = pos
+            pos -= 1
+            i += 1
+pos = 0
+i = 0
+for _ in range(m):
+    d, t = input().split()
+    if d == "R":
+        for j in range(int(t)):
+            b[i] = pos
+            pos += 1
+            i += 1
+    else:
+        for j in range(int(t)):
+            b[i] = pos
+            pos -= 1
+            i += 1
+flag = 1
+for j in range(1,1000000):
+    if a[j] == b[j]:
+        print(j)
+        flag = 0
+        break
+if flag:
+    print(-1)
