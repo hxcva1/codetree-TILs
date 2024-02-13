4113c4ae5815304293944174eb3f09825d8d7b52
Moonsun Park <hxcva1@gmail.com>
2024-02-13 14:00:23 +0900

    Title: 블럭쌓는 명령2, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240213/블럭쌓는 명령2/block-stacking-commands2.py
+++ 240213/블럭쌓는 명령2/block-stacking-commands2.py
@@ -2,7 +2,7 @@ n, k = map(int, input().split())
 arr = [0 for _ in range(n)]
 for _ in range(k):
     a, b = map(int, input().split())
-    for i in range(a,b+1):
+    for i in range(a-1,b):
         arr[i] += 1
 ret = -1
 for i in range(n):
