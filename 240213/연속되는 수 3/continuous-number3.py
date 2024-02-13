3d1672045372aad1a118ff8884248b04202597ab
Moonsun Park <hxcva1@gmail.com>
2024-02-13 20:29:01 +0900

    Title: 연속되는 수 3, Time: 177ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240213/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+arr = [
+    int(input())
+    for _ in range(n)
+]
+
+cnt = 0
+ret = 0
+for i in range(n):
+    if i == 0 or arr[i] * arr[i-1] < 0:
+        cnt = 1
+    else:
+        cnt += 1
+    ret = max(cnt, ret)
+print(ret)
