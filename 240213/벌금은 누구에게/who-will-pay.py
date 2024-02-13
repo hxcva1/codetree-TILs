174665c34942e7f5c4c7d734b948439f9b23c51d
Moonsun Park <hxcva1@gmail.com>
2024-02-13 21:38:44 +0900

    Title: 벌금은 누구에게, Time: 213ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240213/벌금은 누구에게/who-will-pay.py
+++ 240213/벌금은 누구에게/who-will-pay.py
@@ -5,8 +5,9 @@ flag = False
 for _ in range(m):
     num = int(input())
     arr[num] += 1
-    if arr[num] == k:
+    if arr[num] >= k:
         print(num)
         flag = True
+
 if flag == False:
     print(-1)
