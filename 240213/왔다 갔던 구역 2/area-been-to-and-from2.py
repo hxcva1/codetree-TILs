1ecc9b40d58302e5fd70e8beef8eec42bcf78dcd
Moonsun Park <hxcva1@gmail.com>
2024-02-13 14:33:02 +0900

    Title: 왔다 갔던 구역 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240213/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+
+arr = [0] * 2001
+
+now = 1000
+for _ in range(n):
+    x, direction = tuple(input().split())
+    if direction == "R":
+        for i in range(int(x)):
+            arr[now] += 1
+            now += 1
+    else:
+        for i in range(int(x)):
+            arr[now] += 1
+            now -= 1
+ret = 0
+for i in range(2000):
+    if arr[i] >= 2:
+        ret += 1
+print(ret)
