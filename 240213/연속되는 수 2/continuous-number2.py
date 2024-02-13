7c76ca07f7a617c2134795bf7bad4471485bac17
Moonsun Park <hxcva1@gmail.com>
2024-02-13 19:49:17 +0900

    Title: 연속되는 수 2, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240213/연속되는 수 2/continuous-number2.py
+++ 240213/연속되는 수 2/continuous-number2.py
@@ -1,13 +1,14 @@
 n = int(input())
 
 prev = -1
-cnt = 0
+cnt = 1
 ret = 0
 for i in range(n):
     num = int(input())
     if num == 0 or num != prev:
         cnt = 1
         prev = num
+        ret = max(cnt, ret)
         continue
     cnt += 1
     prev = num
