6ad4cf29f2457b455625ff2400a8bdd379d10629
Moonsun Park <hxcva1@gmail.com>
2024-02-13 13:59:59 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240213/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+arr = [0 for _ in range(n)]
+for _ in range(k):
+    a, b = map(int, input().split())
+    for i in range(a,b+1):
+        arr[i] += 1
+ret = -1
+for i in range(n):
+    ret = max(ret, arr[i])
+print(ret)
