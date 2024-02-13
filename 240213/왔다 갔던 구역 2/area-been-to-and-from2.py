df64041035490e49200c03d66ecf9ca75d38b346
Moonsun Park <hxcva1@gmail.com>
2024-02-13 14:34:14 +0900

    Title: 왔다 갔던 구역 2, Time: 115ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240213/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240213/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -10,10 +10,12 @@ for _ in range(n):
         for i in range(int(x)):
             arr[now] += 1
             now += 1
+        now += 1
     else:
         for i in range(int(x)):
             arr[now] += 1
             now -= 1
+        now -= 1
 ret = 0
 for i in range(2000):
     if arr[i] >= 2:
