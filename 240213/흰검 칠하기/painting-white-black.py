7e9e7f1e65906d7fac06fab17731953bfd6014b8
Moonsun Park <hxcva1@gmail.com>
2024-02-13 14:56:18 +0900

    Title: 흰검 칠하기, Time: 133ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240213/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,31 @@
+n = int(input())
+
+
+arr = [0] * 200001
+white = [0] * 200001
+black = [0] * 200001
+
+now = 100000
+for _ in range(n):
+    x, direction = tuple(input().split())
+    if direction == "R":
+        for i in range(int(x)):
+            arr[now] = 1
+            black[now] += 1
+            now += 1
+    else:
+        for i in range(int(x)):
+            now -= 1
+            arr[now] = -1
+            white[now] += 1
+
+w = b = g = 0
+for i in range(200001):
+    if white[i] >= 2 and black[i] >= 2:
+        g += 1
+        continue;
+    if arr[i] > 0:
+        b += 1
+    if arr[i] < 0:
+        w += 1
+print(w, b, g)
