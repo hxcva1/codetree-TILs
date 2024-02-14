86d90438ccae25414effec3ca9faeba5896c861f
Moonsun Park <hxcva1@gmail.com>
2024-02-14 17:16:05 +0900

    Title: 작은 구슬의 이동, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240214/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,24 @@
+n, t = tuple(map(int, input().split()))
+
+r, c, d = input().split()
+
+dxs, dys = [1, 0, -1, 0,], [0, 1, 0, -1]
+mapper = {
+    'U' : 0,
+    'D' : 2,
+    'R' : 1,
+    'L' : 3,
+}
+
+c_dir = mapper[d]
+def in_range(x, y):
+    return 0 < x and x <= n and 0 < y and y <= n
+
+x, y = int(r), int(c)
+for _ in range(t):
+    nx, ny = x + dxs[c_dir], y + dys[c_dir]
+    if not in_range(nx, ny):
+        c_dir = (c_dir + 2) % 4
+        continue
+    x, y = nx, ny
+print(x,y)
