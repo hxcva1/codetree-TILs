efc43cff9b02e5da056944223cb0180f5068c48f
Moonsun Park <hxcva1@gmail.com>
2024-02-17 22:20:50 +0900

    Title: 거울에 레이저 쏘기 2, Time: 168ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240217/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240217/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -12,20 +12,26 @@ for i in range(n):
             arr[i][j] = 1
 
 dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
-
+# 0은 오른쪽, 1은 아래, 2는 왼쪽 3은 위
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
 
 def next_dir(now, mirror):
+    #\일때
     if mirror == 0:
+        #오른쪽
         if now == 0:
             return 1
+        #아래
         elif now == 1:
             return 0
+        #왼쪽
         elif now == 2:
             return 3
+        #위
         else:
             return 2
+    #/일때
     else:
         if now == 0:
             return 3
@@ -35,10 +41,13 @@ def next_dir(now, mirror):
             return 1
         else:
             return 0
+
 x, y = 0, 0
 cnt = 0
 c_dir = 0
 k = int(input())
+
+
 if k // 4 == 0:
     x = 0
     y = (k - 1) % n
@@ -56,8 +65,10 @@ else:
     x = (n - k % n) % n
     c_dir = 0
 
+
 while in_range(x, y):
     cnt += 1
     c_dir = next_dir(c_dir, arr[x][y])
     x, y = x + dxs[c_dir], y + dys[c_dir]
+
 print(cnt)
