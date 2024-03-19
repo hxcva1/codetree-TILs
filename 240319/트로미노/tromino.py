ecbe5c7fd6f70c5a78a0c54f02a3f142f7228af6
Moonsun Park <hxcva1@gmail.com>
2024-03-19 13:37:34 +0900

    Title: 트로미노, Time: 143ms, Memory: 73MB, Status: Wrong Answer - CodeTree

---
+++ 240319/트로미노/tromino.py
@@ -0,0 +1,22 @@
+block = [[(0,0), (1,0), (1,-1)], [(0,0), (1,0), (1,1)], [(0,0), (0,-1), (1,-1)], [(0,0), (0,1), (1,1)], [(0,0), (0,1), (0,2)], [(0,0), (1,0), (2,0)]]
+
+n, m = tuple(map(int, input().split()))
+
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+ret = 0
+for i in range(n):
+    for j in range(n):
+        for k in range(6):
+            cnt = 0
+            for nx, ny in block[k]:
+                if i+nx >= n or j+ny >= n:
+                    cnt = 0
+                    break
+                cnt += arr[i+nx][j+ny]
+            ret = max(ret, cnt)
+
+print(ret)
