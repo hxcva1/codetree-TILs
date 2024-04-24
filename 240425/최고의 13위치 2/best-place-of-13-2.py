15c56b8b3d27861532b38ab0a4adf0fa4719cc15
Moonsun Park <hxcva1@gmail.com>
2024-04-25 00:46:35 +0900

    Title: 최고의 13위치 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240425/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+ans = 0
+for i in range(n):
+    for j in range(n-2):
+        cnt = 0
+        cnt += arr[i][j] + arr[i][j+1] + arr[i][j+2]
+        nx, ny = i, j+1
+        while nx < n:
+            while ny < n-2:
+                cnt2 = 0
+                cnt2 += arr[nx][ny] + arr[nx][ny+1] + arr[nx][ny+2]
+                ans = max(ans, cnt+cnt2)
+                ny += 1
+            nx += 1
+            ny = 0
+print(ans)
