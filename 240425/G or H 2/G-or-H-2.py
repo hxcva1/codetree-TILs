e9ffda9d21af0aabbc0d3b60726ac4f40d6603c9
Moonsun Park <hxcva1@gmail.com>
2024-04-25 02:40:46 +0900

    Title: G or H 2, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240425/G or H 2/G-or-H-2.py
@@ -0,0 +1,32 @@
+MAX_NUM = 100
+
+n = int(input())
+
+arr = [0] * (MAX_NUM + 1)
+
+for _ in range(n):
+    l, a = input().split()
+    l = int(l)
+    if a == "G":
+        arr[l] = 1
+    else:
+        arr[l] = -1
+
+ans = 0
+for i in range(MAX_NUM + 1):
+    if arr[i] != 0:
+        cnt_g = 0
+        cnt_h = 0
+        if arr[i]:
+            cnt_g += 1
+        else:
+            cnt_h += 1
+        for j in range(i+1,MAX_NUM + 1):
+            if arr[j] != 0:
+                if arr[j] == 1:
+                    cnt_g += 1
+                if arr[j] == -1:
+                    cnt_h += 1
+                if cnt_g == cnt_h or cnt_g == 0 or cnt_h == 0:
+                    ans = max(ans, j-i + 1)
+print(ans)
