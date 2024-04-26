61e95eca7f85eb591b486f10d45301d9a6933aeb
Moonsun Park <hxcva1@gmail.com>
2024-04-26 16:32:10 +0900

    Title: 밭의 높이를 고르게하기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240426/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240426/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -7,11 +7,8 @@ arr = list(map(int, input().split()))
 ans = sys.maxsize
 for i in range(n-t+1):
     tmp = arr[i:i+t]
-    mx = max(tmp)
-    mn = min(tmp)
-    for heights in range(mn, mx+1):
-        cost = 0
-        for elem in tmp:
-            cost += abs(elem - heights)
-        ans = min(ans,cost)
+    cost = 0
+    for elem in tmp:
+        cost += abs(elem - h)
+    ans = min(ans,cost)
 print(ans)
