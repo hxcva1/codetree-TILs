cc212913a8580c3da28f3e4f32f7fde97a978bd7
Moonsun Park <hxcva1@gmail.com>
2024-05-02 23:15:06 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 172ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240502/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240502/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -16,7 +16,6 @@ def simulation(x, y, count, max_locs):
         if in_range(nx,ny) and arr[nx][ny] > max_num:
             max_num = arr[nx][ny]
             max_loc = (nx, ny)
-
     max_locs.append(max_loc)
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
