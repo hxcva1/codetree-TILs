37c2ac8e1e75ec93c10c7c01ac9606b17bdde72a
Moonsun Park <hxcva1@gmail.com>
2024-05-02 19:57:21 +0900

    Title: 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240502/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,22 @@
+n, t = tuple(map(int, input().split()))
+
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+
+
+for _ in range(t):
+    tmp1 = arr1[n-1]
+    tmp2 = arr2[n-1]
+    for i in range(n-1, 0, -1):
+        arr1[i] = arr1[i-1]
+    arr1[0] = tmp2
+    for i in range(n-1, 0, -1):
+        arr2[i] = arr2[i-1]
+    arr2[0] = tmp1
+
+for elem in arr1:
+    print(elem, end=" ")
+print()
+for i in n:
+    print(arr2[i], end=" ")
