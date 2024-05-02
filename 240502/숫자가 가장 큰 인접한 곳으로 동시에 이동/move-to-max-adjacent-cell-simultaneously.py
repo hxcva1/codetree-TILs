65a935c2c90be3607e45394f540bcd4c06f2740c
Moonsun Park <hxcva1@gmail.com>
2024-05-02 22:02:43 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240502/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240502/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -5,18 +5,22 @@ arr = [
     for _ in range(n)
 ]
 
-def simulation(x, y, count):
+def simulation(x, y, count, max_locs):
     dxs = [-1,1,0,0]
     dys = [0,0,-1,1]
+    max_num = arr[x][y]
+    max_loc = (x,y)
+    move = False
     for dx, dy in zip(dxs, dys):
         nx = x + dx
         ny = y + dy
-        if in_range(nx,ny) and arr[nx][ny] > arr[x][y]:
-            count[x][y] -= 1 
-            count[nx][ny] += 1
-            return
-
-
+        if in_range(nx,ny) and arr[nx][ny] > max_num:
+            max_num = arr[nx][ny]
+            max_loc = (nx, ny)
+            move = True
+    if move:
+        max_locs.append(max_loc)
+    print()
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
     
@@ -24,15 +28,26 @@ count = [
     [0] * n
     for _ in range(n)
 ]
+
 for _ in range(m):
     r, c = tuple(map(int, input().split()))
     count[r-1][c-1] = 1
 
+
+
 for _ in range(t):
+    max_locs = []
     for i in range(n):
         for j in range(n):
             if count[i][j] == 1:
-                simulation(i,j, count)
+                simulation(i,j, count, max_locs)
+                
+    for i in range(n):
+        for j in range(n):
+            count[i][j] = 0
+    for x, y in max_locs:
+        count[x][y] += 1
+
     for i in range(n):
         for j in range(n):
             if count[i][j] > 1:
