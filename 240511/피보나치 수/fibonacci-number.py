301e2ab942e3e504ce98112d03c348586c55ef05
Moonsun Park <hxcva1@gmail.com>
2024-05-11 16:22:13 +0900

    Title: 피보나치 수, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240511/피보나치 수/fibonacci-number.py
+++ 240511/피보나치 수/fibonacci-number.py
@@ -1,13 +1,15 @@
 n = int(input())
-if n == 1 or n == 2:
-    print(1)
-    exit()
-dp = [0] * (n+1)
 
-dp[1] = 1
-dp[2] = 1
+memo = [-1] * (n+1)
 
-for i in range(3, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+def f(n):
+    if memo[n] != -1:
+        return memo[n]
+    if n <= 2:
+        memo[n] = 1
+        return memo[n]
+    memo[n] = f(n-1) + f(n-2)
+    return memo[n]
 
-print(dp[n])
+
+print(f(n))
