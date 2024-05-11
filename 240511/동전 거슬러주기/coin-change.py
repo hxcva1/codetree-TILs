2c4c878d5056c9411a66b019a2eaab7f1930d92d
Moonsun Park <hxcva1@gmail.com>
2024-05-11 18:16:08 +0900

    Title: 동전 거슬러주기, Time: 175ms, Memory: 25MB, Status: Passed - CodeTree

--- 240511/동전 거슬러주기/coin-change.py
+++ 240511/동전 거슬러주기/coin-change.py
@@ -6,6 +6,7 @@ dp = [2147483647] * (M+1)
 dp[0] = 0
 for i in range(M+1):
     for j in range(N):
+        # 갱신하려는 값이 갱신되지 않은 값을 사용하면 안된다
         if i >= coins[j] and dp[i-coins[j]] != 2147483647:
             dp[i] = min(dp[i], dp[i-coins[j]] + 1)
 if dp[M] == 2147483647:
